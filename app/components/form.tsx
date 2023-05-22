991da68609f790cd9b18f982acbc2ee589aba9b3
ukihot <tokunaga.y@tyco.co.jp>
2023-05-22 16:33:18 +0900

    :art: componentization

--- app/components/form.tsx
+++ app/components/form.tsx
@@ -1,10 +1,28 @@
 import formStyle from '@/styles/components/form.module.scss'
+import { Noto_Sans_JP } from 'next/font/google'
+import { GoRocket } from 'react-icons/go'
+
+const notoSansJP = Noto_Sans_JP({
+    subsets: ['latin'],
+    weight: '100',
+})
 
 const FormPanel = () => {
     return (
-        <section className={formStyle.formSection}>
+        <section>
             <div>
-                <p>form</p>
+                <h2 className={notoSansJP.className}>お問い合わせ</h2>
+            </div>
+            <div className={formStyle.aura}>
+                <div className={formStyle.joinUsButton}>
+                    <GoRocket />
+                    <button
+                        type='submit'
+                        className=''
+                    >
+                        Join Us
+                    </button>
+                </div>
             </div>
         </section>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -3,47 +3,18 @@ import styles from '@/styles/components/page.module.scss'
 import { NextPage } from 'next'
 import dynamic from 'next/dynamic'
 
+const MessagePanel = dynamic(() => import('./components/message'), { ssr: false })
+
+const KabaddiPanel = dynamic(() => import('./components/kabaddi'), { ssr: false })
+
 const FormPanel = dynamic(() => import('./components/form'), { ssr: false })
 
 const WantedPage: NextPage = () => {
     return (
         <main className={styles.main}>
+            <MessagePanel />
+            <KabaddiPanel />
             <FormPanel />
-            <div className={styles.description}>
-                <p>
-                    Get started by editing&nbsp;
-                    <code className={styles.code}>app/page.tsx</code>
-                </p>
-                <div>
-                    <a
-                        href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
-                        By{' '}
-                        <Image
-                            src='/vercel.svg'
-                            alt='Vercel Logo'
-                            className={styles.vercelLogo}
-                            width={100}
-                            height={24}
-                            priority
-                        />
-                    </a>
-                </div>
-            </div>
-
-            <div className={styles.center}>
-                <Image
-                    className={styles.logo}
-                    src='/next.svg'
-                    alt='Next.js Logo'
-                    width={180}
-                    height={37}
-                    priority
-                />
-            </div>
-
             <div className={styles.grid}>
                 <a
                     href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'

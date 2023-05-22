baf9f66e4f81ab83bca14e49887108dd1ab48bf8
ukihot <tokunaga.y@tyco.co.jp>
2023-05-22 12:23:14 +0900

    :fire: remove all file

--- app/components/kabaddiPanel.tsx
+++
@@ -1,9 +0,0 @@
-const KabaddiPanel = () => {
-    return (
-        <section className=" flex-col items-center justify-between border-2 border-solid border-sky-500 p-24">
-            Kabaddi
-        </section>
-    )
-}
-
-export default KabaddiPanel

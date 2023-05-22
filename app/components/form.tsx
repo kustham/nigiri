25a4776eb6332409a377daec718f14d4eb50dfb5
ukihot <tokunaga.y@tyco.co.jp>
2023-05-22 12:38:00 +0900

    :recycle: reconstruction

---
+++ app/components/form.tsx
@@ -0,0 +1,13 @@
+import formStyle from '@/styles/components/form.module.scss'
+
+const FormPanel = () => {
+    return (
+        <section className={formStyle.formSection}>
+            <div>
+                <p>form</p>
+            </div>
+        </section>
+    )
+}
+
+export default FormPanel

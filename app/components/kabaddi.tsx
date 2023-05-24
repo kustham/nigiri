338db540d33a0720ea69c3a22f9f73b98207f783
kustham <ludoludo@outlook.jp>
2023-05-24 23:50:12 +0900

    :lipstick: Update style

--- app/components/kabaddi.tsx
+++ app/components/kabaddi.tsx
@@ -1,11 +1,15 @@
 import KabaddiStyle from '@/styles/components/kabaddi.module.scss'
+import { Train_One } from 'next/font/google'
+
+const trainOne = Train_One({
+    subsets: ['latin'],
+    weight: '400',
+})
 
 const KabaddiPanel = () => {
     return (
         <section className={KabaddiStyle.formSection}>
-            <div>
-                <p>kabaddi</p>
-            </div>
+            <div className={trainOne.className}>kabaddi</div>
         </section>
     )
 }

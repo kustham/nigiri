80e892f022bd650cc8dda1bcedf5cf4fb7f7e1b1
ukihot <tokunaga.y@tyco.co.jp>
2023-05-31 10:20:43 +0900

    :memo:rename team_name

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const oswald = Oswald({ subsets: ['latin'] })
 export const metadata = {
     title: 'Join Us | Kabaddi',
     description:
-        '広島から西日本のカバディを活性化させたい。広島市を拠点とするカバディチーム「ライノセラス」はオープン練習を開催中、未経験者の体験希望を随時募集・歓迎しています。',
+        '広島から西日本のカバディを活性化させたい。広島市を拠点とするカバディチーム「阿國」は、未経験者の体験希望を随時募集・歓迎しています。'
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {

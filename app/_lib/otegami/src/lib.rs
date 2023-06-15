d3c5880e5fd625ab7f22f9823c7b94f7b1b9ff9a
ukihot <tokunaga.y@tyco.co.jp>
2023-06-15 14:10:09 +0900

    :construction: wip

--- app/_lib/otegami/src/lib.rs
+++ app/_lib/otegami/src/lib.rs
@@ -1,19 +1,111 @@
+use std::io::{Read, Write};
+use std::net::TcpStream;
 use wasm_bindgen::prelude::*;
-
-// Import the `window.alert` function from the Web.
+// Import the `window.alert` function SEND_FROM the Web.
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
 }
 
-// Export a `greet` function from Rust to JavaScript, that alerts a
-// hello message.
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+//ts側の使い方memo
+//import init from '@/_lib/otegami/pkg'
+//import * as wasm from '@/_lib/otegami/pkg'
+#[derive(Debug)]
+pub struct SmtpClient {
+    server: String,
+    port: u16,
+    stream: Option<TcpStream>,
+}
+
+impl SmtpClient {
+    pub fn new(server: &str, port: u16) -> Self {
+        Self {
+            server: server.to_string(),
+            port,
+            stream: None,
+        }
+    }
+
+    pub fn connect(&mut self) -> Result<(), String> {
+        let server_address = format!("{}:{}", self.server, self.port);
+        match TcpStream::connect(server_address) {
+            Ok(stream) => {
+                self.stream = Some(stream);
+                Ok(())
+            }
+            Err(err) => Err(err.to_string()),
+        }
+    }
+
+    pub fn send_message(&mut self, subject: &str, body: &str) -> Result<(), String> {
+        const SEND_FROM: &str = "4.togenkyo@gmail.com";
+        const SEND_TO: &str = "4.togenkyo@gmail.com";
+        self.read_response()?;
+
+        self.send_command(format!("HELO {}\r\n", self.server))?;
+        self.read_response()?;
+
+        self.send_command(format!("MAIL FROM:<{}>\r\n", SEND_FROM))?;
+        self.read_response()?;
+
+        self.send_command(format!("RCPT TO:<{}>\r\n", SEND_TO))?;
+        self.read_response()?;
+
+        self.send_command("DATA\r\n".to_string())?;
+        self.read_response()?;
+
+        let mail_header = format!(
+            "From: {}\r\nTo: {}\r\nSubject: {}\r\n\r\n",
+            SEND_FROM, SEND_TO, subject
+        );
+        self.send_command(mail_header)?;
+
+        self.send_command(body.to_string())?;
+
+        self.send_command("\r\n.\r\n".to_string())?;
+        self.read_response()?;
+
+        self.send_command("QUIT\r\n".to_string())?;
+        self.read_response()?;
+
+        Ok(())
+    }
+
+    fn send_command(&mut self, command: String) -> Result<(), String> {
+        match &mut self.stream {
+            Some(stream) => match stream.write_all(command.as_bytes()) {
+                Ok(_) => Ok(()),
+                Err(err) => Err(err.to_string()),
+            },
+            None => Err("Not connected to the server.".to_string()),
+        }
+    }
+
+    fn read_response(&mut self) -> Result<String, String> {
+        let mut buffer = [0; 512];
+        match &mut self.stream {
+            Some(stream) => match stream.read_exact(&mut buffer) {
+                Ok(_) => {
+                    let response = String::from_utf8_lossy(&buffer).to_string();
+                    Ok(response)
+                }
+                Err(err) => Err(err.to_string()),
+            },
+            None => Err("Not connected to the server.".to_string()),
+        }
+    }
 }
 
 #[wasm_bindgen]
-pub fn otegami_add(a: i32, b: i32) -> i32 {
-    a + b
+pub fn send_email(subject: &str, body: &str) -> Result<(), JsValue> {
+    const SMTP_SERVER: &str = "smtp.gmail.com";
+    const SMTP_PORT: u16 = 587;
+
+    let mut client = SmtpClient::new(SMTP_SERVER, SMTP_PORT);
+    client.connect().map_err(|err| JsValue::from_str(&err))?;
+    client
+        .send_message(subject, body)
+        .map_err(|err| JsValue::from_str(&err))?;
+
+    Ok(())
 }
--- app/recruit/sendMailPanel.tsx
+++ app/recruit/sendMailPanel.tsx
@@ -1,36 +1,64 @@
-import { greet } from '@/_lib/otegami/pkg/otegami'
 import { NextPage } from 'next'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import styles from 'styles/recruit/recruit.module.scss'
-
 type Props = {
     // rome-ignore lint/suspicious/noExplicitAny: <explanation>
     close?: (e: any) => void
 }
 
-// 入力フォームの型定義
 type FormValues = {
-    password: string
-    confirmPassword: string
+    name: string
+    email: string
+    message: string
 }
 
 const SendMailPanel: NextPage<Props> = (props) => {
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm<FormValues>()
+
+    const onSubmit: SubmitHandler<FormValues> = (data) => {
+        /*
+        fetch('/api/send', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: 'test@exsample.com',
+                message: { ...data },
+            }),
+        })
+        */
+        console.log('finish')
+    }
+
     return (
-        <form className={styles.panel}>
+        <form className={styles.panel} onSubmit={handleSubmit(onSubmit)}>
             <label>お問い合わせ</label>
-            <div />
-            <footer>
+            <div>
+                <input {...register('name')} />
+                <input
+                    {...register('email', {
+                        required: true,
+                        maxLength: 60,
+                        pattern: {
+                            value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                            message: 'メールアドレスの形式が不正です',
+                        },
+                    })}
+                />
+                <input {...register('message')} />
+            </div>
+            <div className={styles.formFooter}>
                 <button type='button' onClick={props.close}>
                     Cancel
                 </button>
-                <button
-                    type='submit'
-                    onClick={(e) => {
-                        greet('test')
-                    }}
-                >
-                    Send
-                </button>
-            </footer>
+                <button type='submit'>Send</button>
+            </div>
         </form>
     )
 }

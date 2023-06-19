2ec5c1bee271cefacd969349cf4ba3e280b14e18
kustham <ludoludo@outlook.jp>
2023-06-19 23:15:04 +0900

    :lipstick: update style

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,19 +1,31 @@
 'use client'
 import headerStyles from 'styles/components/header.module.scss'
-import Image from 'next/image'
 import Link from 'next/link'
 import { ChangeThemeButton } from './theme/themeButton'
+import { Unbounded, Oxanium } from 'next/font/google'
+
+const unbounded = Unbounded({
+    subsets: ['latin'],
+    weight: '800',
+})
+
+const oxanium = Oxanium({
+    subsets: ['latin'],
+    weight: '500',
+})
 
 const Header = () => {
     return (
         <header className={headerStyles.agniHeader}>
             <nav className={headerStyles.agniNav}>
                 <Link href='/'>
-                    <Image src='/logo.svg' width={140} height={140} alt='logo' />
+                    <h1 className={`${headerStyles.teamName} ${unbounded.className}`}>AGNI HIROSHIMA KABADDI TEAM</h1>
                 </Link>
-                <ul className={headerStyles.navMenu}>
+                <ul className={`${headerStyles.navMenu} ${oxanium.className}`}>
                     <li className={headerStyles.joinus}>
-                        <Link href='/recruit'>Join Us</Link>
+                        <Link href='/recruit'>
+                            Join Us
+                        </Link>
                     </li>
                     <li>
                         <ChangeThemeButton />

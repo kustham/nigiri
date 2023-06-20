f8b4c6dc0425ae19003fabd47c42491c92d93d83
ukihot <tokunaga.y@tyco.co.jp>
2023-06-20 15:58:35 +0900

    :construction: wip

--- app/_components/kabaddi/court.tsx
+++ app/_components/kabaddi/court.tsx
@@ -5,7 +5,8 @@ const CoreRule = () => {
     return (
         <>
             <div className={kabaddiStyles.ruleTitle}>
-                <h2>Court</h2>
+                <h2 className={kabaddiStyles.title}>Court
+                </h2>
                 <br />
                 <p>以下のコート内で7人ずつに分かれて戦います。</p>
             </div>

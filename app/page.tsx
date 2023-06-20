78e32b075cef399a7e3ee7f5d4d8a87f66a997d8
kustham <ludoludo@outlook.jp>
2023-06-21 00:15:25 +0900

    :construction: wip

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,17 @@ import { NextPage } from 'next'
 import '@/styles/globals.scss'
 import KabaddiBoard from './_components/kabaddi'
 import SignBoard from './_components/sign'
+import Footer from './_components/footer'
 
 const Home: NextPage = () => {
     return (
-        <main className='main'>
-            <SignBoard />
-            <KabaddiBoard />
-        </main>
+        <>
+            <main className='main'>
+                <SignBoard />
+                <KabaddiBoard />
+            </main>
+            <Footer />
+        </>
     )
 }
 
--- app/recruit/page.tsx
+++ app/recruit/page.tsx
@@ -6,6 +6,7 @@ import { NextPage } from 'next'
 import { Righteous } from 'next/font/google'
 import { useState } from 'react'
 import { LightningBoltIcon } from '@radix-ui/react-icons'
+import Footer from '@/_components/footer'
 
 const righteous = Righteous({
     subsets: ['latin'],
@@ -21,66 +22,65 @@ const RecruitPage: NextPage = () => {
         }
     }
     return (
-        <>
-            <div className={recruitStyle.message}>
-                <div className={recruitStyle.recruitItem}>
-                    <h1 className={recruitStyle.subTitle}>プレイヤーの公募について</h1>
-                    <div className={recruitStyle.pr}>
-                        <p>
-                            新規に競技を始められる方のほとんどは大学生以上です。体が自由に動くのであれば，カバディは誰でも始めやすいスポーツです。
-                        </p>
-                        <p>
-                            カバディの起源は野性的な狩猟にあり，極限の緊張下で取り組まれる駆け引きです。もし，このページをご覧になっているあなたがまだカバディをプレイしたことがなければ，一度私たちの練習に参加してみませんか？
-                        </p>
-                    </div>
+        <div className={recruitStyle.message}>
+            <div className={recruitStyle.recruitItem}>
+                <h1 className={recruitStyle.subTitle}>プレイヤーの公募について</h1>
+                <div className={recruitStyle.pr}>
+                    <p>
+                        新規に競技を始められる方のほとんどは大学生以上です。体が自由に動くのであれば，カバディは誰でも始めやすいスポーツです。
+                    </p>
+                    <p>
+                        カバディの起源は野性的な狩猟にあり，極限の緊張下で取り組まれる駆け引きです。
+                    </p>
+                    <p>もし，このページをご覧になっているあなたがまだカバディをプレイしたことがなければ，一度私たちの練習に参加してみませんか？</p>
                 </div>
-                <div className={recruitStyle.recruitItem}>
-                    <h2 className={recruitStyle.subTitle}>募集要項</h2>
-                    <table className={recruitStyle.conditions}>
-                        <tbody>
-                            <tr className={recruitStyle.conditionsCol}>
-                                <th className={recruitStyle.conditionsRowTop}>ご年齢</th>
-                                <td className={recruitStyle.conditionsRow}>18&nbsp;歳以上</td>
-                            </tr>
-                            <tr className={recruitStyle.conditionsCol}>
-                                <th className={recruitStyle.conditionsRowTop}>定期練習</th>
-                                <td className={recruitStyle.conditionsRow}>月に1&nbsp;回以上の参加が可能な方</td>
-                            </tr>
-                            <tr className={recruitStyle.conditionsCol}>
-                                <th className={recruitStyle.conditionsRowTop}>大会</th>
-                                <td className={recruitStyle.conditionsRow}>
-                                    毎年秋の西日本大会(広島市)へご参加いただける方
-                                </td>
-                            </tr>
-                            <tr className={recruitStyle.conditionsCol}>
-                                <th className={recruitStyle.conditionsRowTop}>スポーツ経験</th>
-                                <td className={recruitStyle.conditionsRow}>不問</td>
-                            </tr>
-                            <tr className={recruitStyle.conditionsCol}>
-                                <th className={recruitStyle.conditionsRowTop}>体重</th>
-                                <td className={recruitStyle.conditionsRow}>85&nbsp;Kg未満</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div className={recruitStyle.buttonContainer}>
-                    <span className={recruitStyle.aura} />
-                    <button
-                        type='submit'
-                        className={`${recruitStyle.joinUsButton} ${righteous.className}`}
-                        onClick={toggleModal}
-                    >
-                        Join Agni
-                        <LightningBoltIcon />
-                    </button>
-                </div>
+            </div>
+            <div className={recruitStyle.recruitItem}>
+                <h2 className={recruitStyle.subTitle}>募集要項</h2>
+                <table className={recruitStyle.conditions}>
+                    <tbody>
+                        <tr className={recruitStyle.conditionsCol}>
+                            <th className={recruitStyle.conditionsRowTop}>ご年齢</th>
+                            <td className={recruitStyle.conditionsRow}>18&nbsp;歳以上</td>
+                        </tr>
+                        <tr className={recruitStyle.conditionsCol}>
+                            <th className={recruitStyle.conditionsRowTop}>定期練習</th>
+                            <td className={recruitStyle.conditionsRow}>月に1&nbsp;回以上の参加が可能な方</td>
+                        </tr>
+                        <tr className={recruitStyle.conditionsCol}>
+                            <th className={recruitStyle.conditionsRowTop}>大会</th>
+                            <td className={recruitStyle.conditionsRow}>
+                                毎年秋の西日本大会(広島市)へご参加いただける方
+                            </td>
+                        </tr>
+                        <tr className={recruitStyle.conditionsCol}>
+                            <th className={recruitStyle.conditionsRowTop}>スポーツ経験</th>
+                            <td className={recruitStyle.conditionsRow}>不問</td>
+                        </tr>
+                        <tr className={recruitStyle.conditionsCol}>
+                            <th className={recruitStyle.conditionsRowTop}>体重</th>
+                            <td className={recruitStyle.conditionsRow}>85&nbsp;Kg未満</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div className={recruitStyle.buttonContainer}>
+                <span className={recruitStyle.aura} />
+                <button
+                    type='submit'
+                    className={`${recruitStyle.joinUsButton} ${righteous.className}`}
+                    onClick={toggleModal}
+                >
+                    Join Agni
+                    <LightningBoltIcon />
+                </button>
             </div>
             {isOpenModal && (
                 <Modal close={toggleModal}>
                     <SendMailPanel />
                 </Modal>
             )}
-        </>
+        </div>
     )
 }
 

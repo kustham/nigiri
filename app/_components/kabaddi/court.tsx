156f69ca6b54bd33768bbfe979d26ef83113b371
ukihot <tokunaga.y@tyco.co.jp>
2023-06-21 11:24:07 +0900

    :art: brush up pr

--- app/_components/kabaddi/court.tsx
+++ app/_components/kabaddi/court.tsx
@@ -5,10 +5,12 @@ const CoreRule = () => {
     return (
         <>
             <div className={kabaddiStyles.ruleTitle}>
-                <h2 className={kabaddiStyles.title}>Court
-                </h2>
+                <h2 className={kabaddiStyles.title}>Court</h2>
                 <br />
-                <p>以下のコート内で7人ずつに分かれて戦います。</p>
+                <p>以下のコート内で７人ずつ，２０分ハーフで戦います。</p>
+                <p>
+                    敵陣に１人エントリーすることで攻撃（レイド）が始まります。レイドの制限時間は３０秒以内と定められています。
+                </p>
             </div>
 
             <CourtModel />
--- app/_components/kabaddi/raid.tsx
+++ app/_components/kabaddi/raid.tsx
@@ -6,32 +6,33 @@ const RaidRule = () => {
             <div className={kabaddiStyles.ruleTitle}>
                 <h2>Raid</h2>
                 <br />
-                <p>敵にタッチして自陣に帰還することで，タッチした人数分を得点できます。</p>
+                <p>レイダーが敵にタッチして自陣に帰還することで，タッチした人数分を得点できます。</p>
+                <p>タッチされた敵はアウトとなり，コート外待機します。</p>
             </div>
             <div className={kabaddiStyles.ruleContainer}>
                 <div>
                     <h3 className={kabaddiStyles.raidRule}>キャント - CANT</h3>
                     <p>
-                        敵陣にいる間は「カバディ」と発声し続ける。激しく連呼する必要はないが，タックルされて呻き声があがってしまったり，くしゃみ等の別の音を発するとアウトになる．
+                        敵陣にいる間は「カバディ」と発声し続ける。激しく連呼する必要はないが，タックルされて呻き声があがってしまった等の別の音を発するとアウトになる。
                     </p>
                 </div>
-                <div className={kabaddiStyles.raidRule}>
-                    <h3>ストラグル - STRUGGLE</h3>
+                <div>
+                    <h3 className={kabaddiStyles.raidRule}>ストラグル - STRUGGLE</h3>
                     <p>
-                        敵アンティに触れた瞬間から自陣に戻るまでの時間をストラグルという。
+                        敵に触れた瞬間から自陣に戻るまでの時間をストラグルという。
                         ストラグル中のコートはロビーまでを含める。
                     </p>
                 </div>
-                <div className={kabaddiStyles.raidRule}>
-                    <h3>リターン - RETURN</h3>
+                <div>
+                    <h3 className={kabaddiStyles.raidRule}>帰還 - RETURN</h3>
                     <p>
-                        体の一部がミッドラインを越えることで自分の陣地に帰ったことになる。
+                        体の一部がミッドラインを越えることで自分の陣地に帰還することができる。ストラグルが発生した場合はこのとき得点する。
                         レイド中にボークラインを越えていれば，ストラグル無しでも帰ることができ、これをエンプティレイドという。
-                        エンプティレイドは連続2回まで認められ，次のサードレイド（Do-or-Die）でエンプティレイドをしてしまうとアウトになる。
+                        ただし，エンプティレイドを3回連続行うとRaiderがアウトになる。また，アウトにした敵の人数分，アウトになっている味方がコートに復活する。
                     </p>
                 </div>
-                <div className={kabaddiStyles.raidRule}>
-                    <h3>ボーナス - BONUS</h3>
+                <div>
+                    <h3 className={kabaddiStyles.raidRule}>ボーナス - BONUS</h3>
                     <p>アンティが6人以上のときにボーナスラインの先に入ることで，1点得点できる。</p>
                 </div>
             </div>

6625d20ba7bc3d9cad5803c09875f50df8403d01
kustham <ludoludo@outlook.jp>
2023-07-02 00:15:21 +0900

    :lipstick: Logo with a hazy cloud behind.

--- app/_components/sign.tsx
+++ app/_components/sign.tsx
@@ -1,6 +1,14 @@
+import Image from 'next/image'
 import styles from 'styles/components/sign.module.scss'
+
 const SignBoard = () => {
-    return <section className={styles.signSection} />
+    return (
+        <section className={styles.signSection}>
+            <div className={styles.center}>
+                <Image className={styles.logo} src='/logo.svg' alt='Agni Logo' fill priority />
+            </div>
+        </section>
+    )
 }
 
 export default SignBoard
